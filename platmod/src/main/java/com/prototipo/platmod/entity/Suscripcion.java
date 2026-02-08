e995b3d102000487bba6b5d6aecfa8f54f594f15
Jaime-MV <jaime.a.matamoros@gmail.com>
2026-02-08 00:12:32 -0600

    Correccion Suscripcion

--- platmod/src/main/java/com/prototipo/platmod/entity/Suscripcion.java
+++ platmod/src/main/java/com/prototipo/platmod/entity/Suscripcion.java
@@ -1,4 +1,4 @@
-package com.tudominio.cursosapi.entity;
+package com.prototipo.platmod.entity;
 
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotNull;
@@ -14,27 +14,27 @@ import java.time.LocalDate;
 @NoArgsConstructor
 @AllArgsConstructor
 public class Suscripcion {
-    
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idSuscripcion;
-    
+
     @ManyToOne
     @JoinColumn(name = "id_estudiante", nullable = false)
     private Estudiante estudiante;
-    
+
     @ManyToOne
     @JoinColumn(name = "id_plan", nullable = false)
     private PlanSuscripcion plan;
-    
+
     @NotNull(message = "La fecha de inicio es obligatoria")
     @Column(name = "fecha_inicio", nullable = false)
     private LocalDate fechaInicio;
-    
+
     @NotNull(message = "La fecha de fin es obligatoria")
     @Column(name = "fecha_fin", nullable = false)
     private LocalDate fechaFin;
-    
+
     @Column(nullable = false)
     private Boolean estado = true;
 }
